d516bd87f7fecadce451c82fa28bd2fdca83fc2d
ShreeSivakumar <shree14003@gmail.com>
2025-11-04 06:10:53 -0500

    Add latest files

---
+++ followersCount/followers_count.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+function displayCount() {
+document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+}
+
+function checkCountValue(){
+    if(count === 10){
+        alert("You gained 10 followers")
+    }
+    else if(count === 20){
+        alert("You gained 20 followers")
+    }
+}
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
